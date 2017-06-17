de22cdc4e9d76ecce12ea5f632dacdeadd82d08e
Greg G <m-ae.j@outlook.com>
2017-06-17 18:33:49 +0200

    Now cleaning up attributes

--- src/Cometary/Internal/Visitors/AttributesVisitor.cs
+++ src/Cometary/Internal/Visitors/AttributesVisitor.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using TypeInfo = System.Reflection.TypeInfo;
@@ -22,130 +24,189 @@ namespace Cometary
             return member == null ? Enumerable.Empty<T>() : member.GetCustomAttributes().OfType<T>();
         }
 
+        private static SyntaxList<AttributeListSyntax> RemoveAttribute(SyntaxList<AttributeListSyntax> attributes, IList<string> attrsToRemove)
+        {
+            for (int i = 0; i < attributes.Count; i++)
+            {
+                var attrs = attributes[i].Attributes;
+
+                for (int o = 0; o < attrs.Count; o++)
+                {
+                    AttributeSyntax attr = attrs[o];
+
+                    if (attr.Name is SimpleNameSyntax name &&
+                        (attrsToRemove.IndexOf(name.Identifier.Text) != -1 ||
+                         attrsToRemove.IndexOf(name.Identifier.Text + nameof(Attribute)) != -1))
+                        attrs = attrs.RemoveAt(o--);
+                }
+
+                if (attrs.Count == 0)
+                    attributes = attributes.RemoveAt(i--);
+            }
+
+            return attributes;
+        }
+
         /// <inheritdoc />
         public override MethodDeclarationSyntax Visit(MethodInfo method, MethodDeclarationSyntax node)
         {
+            List<string> attrsToRemove = new List<string>();
+
             foreach (IMethodVisitor visitor in GetCustomAttributes<IMethodVisitor>(method))
             {
                 node = visitor.Visit(method, node);
 
                 if (node == null)
                     return null;
+
+                attrsToRemove.Add(visitor.GetType().Name);
             }
 
-            return node;
+            return node.WithAttributeLists(RemoveAttribute(node.AttributeLists, attrsToRemove));
         }
 
         /// <inheritdoc />
         public override ClassDeclarationSyntax Visit(TypeInfo type, ClassDeclarationSyntax node)
         {
+            List<string> attrsToRemove = new List<string>();
+
             foreach (ITypeVisitor visitor in GetCustomAttributes<ITypeVisitor>(type))
             {
                 node = visitor.Visit(type, node);
 
                 if (node == null)
                     return null;
+
+                attrsToRemove.Add(visitor.GetType().Name);
             }
 
-            return node;
+            return node.WithAttributeLists(RemoveAttribute(node.AttributeLists, attrsToRemove));
         }
 
         /// <inheritdoc />
         public override PropertyDeclarationSyntax Visit(PropertyInfo property, PropertyDeclarationSyntax node)
         {
+            List<string> attrsToRemove = new List<string>();
+
             foreach (IPropertyVisitor visitor in GetCustomAttributes<IPropertyVisitor>(property))
             {
                 node = visitor.Visit(property, node);
 
                 if (node == null)
                     return null;
+
+                attrsToRemove.Add(visitor.GetType().Name);
             }
 
-            return node;
+            return node.WithAttributeLists(RemoveAttribute(node.AttributeLists, attrsToRemove));
         }
 
         /// <inheritdoc />
         public override FieldDeclarationSyntax Visit(FieldInfo field, FieldDeclarationSyntax node)
         {
+            List<string> attrsToRemove = new List<string>();
+
             foreach (IFieldVisitor visitor in GetCustomAttributes<IFieldVisitor>(field))
             {
                 node = visitor.Visit(field, node);
 
                 if (node == null)
                     return null;
+
+                attrsToRemove.Add(visitor.GetType().Name);
             }
 
-            return node;
+            return node.WithAttributeLists(RemoveAttribute(node.AttributeLists, attrsToRemove));
         }
 
         /// <inheritdoc />
         public override DelegateDeclarationSyntax Visit(TypeInfo type, DelegateDeclarationSyntax node)
         {
+            List<string> attrsToRemove = new List<string>();
+
             foreach (IDelegateVisitor visitor in GetCustomAttributes<IDelegateVisitor>(type))
             {
                 node = visitor.Visit(type, node);
 
                 if (node == null)
                     return null;
+
+                attrsToRemove.Add(visitor.GetType().Name);
             }
 
-            return node;
+            return node.WithAttributeLists(RemoveAttribute(node.AttributeLists, attrsToRemove));
         }
 
         /// <inheritdoc />
-        public override MethodDeclarationSyntax Visit(ParameterInfo parameter, ParameterSyntax syntax, MethodDeclarationSyntax node1)
+        public override MethodDeclarationSyntax Visit(ParameterInfo parameter, ParameterSyntax syntax, MethodDeclarationSyntax node)
         {
+            List<string> attrsToRemove = new List<string>();
+
             foreach (IParameterVisitor visitor in parameter.GetCustomAttributes().OfType<IParameterVisitor>())
             {
-                node1 = visitor.Visit(parameter, syntax, node1);
+                node = visitor.Visit(parameter, syntax, node);
 
-                if (node1 == null)
+                if (node == null)
                     return null;
+
+                attrsToRemove.Add(visitor.GetType().Name);
             }
 
-            return node1;
+            return node.WithAttributeLists(RemoveAttribute(node.AttributeLists, attrsToRemove));
         }
 
         /// <inheritdoc />
         public override EventDeclarationSyntax Visit(EventInfo @event, EventDeclarationSyntax node)
         {
+            List<string> attrsToRemove = new List<string>();
+
             foreach (IEventVisitor visitor in GetCustomAttributes<IEventVisitor>(@event))
             {
                 node = visitor.Visit(@event, node);
 
                 if (node == null)
                     return null;
+
+                attrsToRemove.Add(visitor.GetType().Name);
             }
 
-            return node;
+            return node.WithAttributeLists(RemoveAttribute(node.AttributeLists, attrsToRemove));
         }
 
         /// <inheritdoc />
         public override EnumDeclarationSyntax Visit(TypeInfo @enum, EnumDeclarationSyntax node)
         {
+            List<string> attrsToRemove = new List<string>();
+
             foreach (IEnumVisitor visitor in GetCustomAttributes<IEnumVisitor>(@enum))
             {
                 node = visitor.Visit(@enum, node);
 
                 if (node == null)
                     return null;
+
+                attrsToRemove.Add(visitor.GetType().Name);
             }
 
-            return node;
+            return node.WithAttributeLists(RemoveAttribute(node.AttributeLists, attrsToRemove));
         }
 
         /// <inheritdoc />
         public override InterfaceDeclarationSyntax Visit(TypeInfo @interface, InterfaceDeclarationSyntax node)
         {
+            List<string> attrsToRemove = new List<string>();
+
             foreach (IInterfaceVisitor visitor in GetCustomAttributes<IInterfaceVisitor>(@interface))
             {
                 node = visitor.Visit(@interface, node);
 
                 if (node == null)
                     return null;
+
+                attrsToRemove.Add(visitor.GetType().Name);
             }
 
-            return node;
+            return node.WithAttributeLists(RemoveAttribute(node.AttributeLists, attrsToRemove));
         }
 
         /// <inheritdoc />
